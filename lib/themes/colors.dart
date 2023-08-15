eb6b1a3760c1a1ae5a5bc237acb826b7d97f1ab0
AnthonyBeerus <morima.anthony@gmail.com>
2023-08-15 22:14:34 +0200

    Color change

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 var primaryColor = const Color(0xFFF7F4F3);
 var secondaryColor = const Color(0xFF0892A5);
-var thirdColor = const Color(0xFFE65F5C);
+var thirdColor = const Color(0xFF0BD0EA);

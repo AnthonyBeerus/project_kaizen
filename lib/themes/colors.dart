0c52e68d5baabd82f6581bfc71b716080ae9015e
AnthonyBeerus <morima.anthony@gmail.com>
2023-08-15 21:16:24 +0200

    New Colors

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
 var primaryColor = const Color(0xFFF7F4F3);
-var secondaryColor = const Color(0xFF5B2333);
+var secondaryColor = const Color(0xFF0892A5);
+var thirdColor = const Color(0xFFE65F5C);

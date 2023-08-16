12464599481b089ec4c9c60f1bdc40865242e973
AnthonyBeerus <morima.anthony@gmail.com>
2023-08-16 10:32:27 +0200

    Decoupled floating action button behaviour from these files

--- lib/pages/message.dart
+++ lib/pages/message.dart
@@ -2,41 +2,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:phenom_fitness/themes/colors.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 
-class Message extends StatefulWidget {
-  const Message({super.key});
+class MessageScreen extends StatefulWidget {
+  const MessageScreen({super.key});
 
   @override
-  State<Message> createState() => _MessageState();
+  State<MessageScreen> createState() => _MessageScreenState();
 }
 
-class _MessageState extends State<Message> {
+class _MessageScreenState extends State<MessageScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SpeedDial(
-          backgroundColor: secondaryColor,
-          animatedIcon: AnimatedIcons.view_list,
-          overlayOpacity: 0.4,
-          spacing: 10,
-          spaceBetweenChildren: 10,
-          children: [
-            SpeedDialChild(
-              backgroundColor: secondaryColor,
-              child:
-               const Icon(Icons.help, color: Colors.white,),
-            ),
-            SpeedDialChild(
-              backgroundColor: secondaryColor,
-              child: const Icon(Icons.history, color: Colors.white,)
-            )
-          ],
-        ),
-      ),
       backgroundColor: primaryColor,
       body: Center(
         child: Text(
--- lib/pages/workout.dart
+++ lib/pages/workout.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:phenom_fitness/themes/colors.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class Workout extends StatefulWidget {
   const Workout({super.key});
@@ -15,28 +14,6 @@ class _WorkoutState extends State<Workout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SpeedDial(
-          backgroundColor: secondaryColor,
-          overlayColor: primaryColor,
-          overlayOpacity: 0.4,
-          animatedIcon: AnimatedIcons.view_list,
-          spacing: 10,
-          spaceBetweenChildren: 10,
-          children: [
-            SpeedDialChild(
-              backgroundColor: secondaryColor,
-              child: const Icon(Icons.bar_chart, color: Colors.white,),
-              labelWidget: const Text(
-                'Workout History',
-                ),
-            ),
-          ],
-        ),
-      ),
-      
       backgroundColor: primaryColor,
       body: Center(
         child: Text(

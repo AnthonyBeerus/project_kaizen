efe7e2ba9fee6e0986ed60435cc7b72b88df7b32
AnthonyBeerus <morima.anthony@gmail.com>
2023-08-22 21:43:58 +0200

    Added more descriptive comments

--- lib/Domain/Models/exercise_class.dart
+++ lib/Domain/Models/exercise_class.dart
@@ -1,15 +1,26 @@
+// The ExerciseClass represents an individual exercise within a workout.
 class ExerciseClass {
+  // The name of the exercise, such as "Bicep Curls" or "Squats".
   final String name;
+
+  // The weight used for the exercise, e.g., "10 kg" or "50 lbs".
   final String weight;
+
+  // The number of sets for the exercise, e.g., "4 sets".
   final String sets;
+
+  // The number of repetitions (reps) per set for the exercise, e.g., "10 reps".
   final String reps;
+
+  // Indicates whether the exercise has been completed or not.
   bool isCompleted;
 
+  // Constructor to initialize an ExerciseClass instance.
   ExerciseClass({
-    required this.name,
-    required this.weight,
-    required this.sets,
-    required this.reps,
-    this.isCompleted = false,
+    required this.name, // The name of the exercise is required.
+    required this.weight, // The weight used is required.
+    required this.sets, // The number of sets is required.
+    required this.reps, // The number of reps is required.
+    this.isCompleted = false, // Whether the exercise is completed (default is false).
   });
 }

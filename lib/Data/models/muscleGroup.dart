bd201cbe707fe4f944d6aca860ae1be577d05ddb
AnthonyBeerus <morima.anthony@gmail.com>
2023-09-04 13:45:19 +0200

    Add dictionary for Muscle groups and Specific Muscles

---
+++ lib/Data/models/muscleGroup.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+Map<String, List<String>> muscleGroups = {
+  "Abs": ["Abdominis", "Obliques"],
+  "Arms": ["Biceps", "Forearms", "Triceps",],
+  "Back": ["Lats","Lower Back", "Traps", "Upper Back" ],
+  "Chest": ["Pectoralis Major", "Pectoralis Minor"],
+  "Legs": ["Calves", "Glutes", "Hamstrings", "Quadriceps" ],
+  "Shoulders": ["Front Deltoids", "Side Deltoids", "Rear Deltoids"],
+};

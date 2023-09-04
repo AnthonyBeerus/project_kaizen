62fcddaffc68a7d47a71ae4ead58b0d917f83f1f
AnthonyBeerus <morima.anthony@gmail.com>
2023-09-04 22:34:45 +0200

    meh

--- lib/Data/models/muscleGroup.dart
+++ lib/Data/models/muscleGroup.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 
 
-Map<String, List<String>> muscleGroups = {
-  "Abs": ["Abdominis", "Obliques"],
-  "Arms": ["Biceps", "Forearms", "Triceps",],
-  "Back": ["Lats","Lower Back", "Traps", "Upper Back" ],
-  "Chest": ["Pectoralis Major", "Pectoralis Minor"],
-  "Legs": ["Calves", "Glutes", "Hamstrings", "Quadriceps" ],
-  "Shoulders": ["Front Deltoids", "Side Deltoids", "Rear Deltoids"],
+Map<String, Map<String, List<String>>> muscleGroups = {
+  "Abs": {
+    "Abdominals":["Crunches", "Sit-Ups"],
+  },
+  "Arms": {
+    "Biceps": ["Bicep Curls", "Hammer Curls", "Preacher Curls"],
+    "Triceps": ["Tricep Extensions", "Close-Grip Bench Press", "Overhead Triceps Extensions"],
+    "Forearms": ["Wrist Curls", "Reverse Wrist Curls", "Plate Pinches"]
+  },
+  "Back": {
+    "Lats": ["Pull-ups", "Dumbbell Rows", "Pulldowns"],
+    "Lower Back": ["Deadlifts", "Good Mornings", "Hyperextensions"],
+    "Traps": ["Barbell Shrugs", "Dumbbell Shrugs", "Cable Shrugs"],
+    "Upper Back": ["Face Pulls", "Band Pull-Aparts", "Rows"]
+  },
+  "Chest": {
+    "Pectoralis Major": ["Bench Press", "Incline Bench Press", "Decline Bench Press"],
+    "Pectoralis Minor": ["Flyes", "Cable Crossovers", "Chest Dips"]
+  },
+  "Legs": {
+    "Quadriceps": ["Squats", "Leg Press", "Leg Extensions"],
+    "Hamstrings": ["Deadlifts", "Romanian Deadlifts", "Hamstring Curls"],
+    "Calves": ["Calf Raises", "Seated Calf Raises", "Standing Calf Raises"],
+    "Glutes": ["Hip Thrusts", "Glute Bridges", "Squats"]
+  },
+  "Shoulders": {
+    "Front Deltoids": ["Overhead Press", "Front Raises", "Lateral Raises"],
+    "Side Deltoids": ["Lateral Raises", "Lateral Raises to Front", "Face Pulls"],
+    "Rear Deltoids": ["Rear Delt Flyes", "Face Pulls", "Reverse Flyes"]
+  },
 };

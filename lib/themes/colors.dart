31881530f39efae15f6e0190ce3dcc7360b0ada4
AnthonyBeerus <morima.anthony@gmail.com>
2023-09-04 05:57:50 +0200

    name change

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-var secondaryColor = const Color(0xFFEAE2B7);
-var thirdColor = const Color(0xFFFcbf49);
-var primaryColor = const Color(0xFFF77F00);
+var secondaryColor = const Color(0xFFF6F7EB);
+var primaryColor = const Color(0xFF393E41);
+var thirdColor = const Color(0xFFE94F37);
 var backgroundColor = const Color(0xFFD5E4EB);
 var textColor = const Color(0xFF140900);

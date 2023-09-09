16c24893912582fd73f6eacd65f0912d2ae431d6
AnthonyBeerus <morima.anthony@gmail.com>
2023-09-09 17:38:42 +0200

    Coded Checkbox method and changed widget tree order

--- lib/Presentation Layer/pages/routine.dart
+++ lib/Presentation Layer/pages/routine.dart
@@ -13,40 +13,70 @@ class Routine extends StatefulWidget {
 }
 
 class _RoutineState extends State<Routine> {
+  //Check box was tapped
+  void onCheckBoxChanged(String workoutName, String exerciseName) {
+    Provider.of<WorkoutData>(context, listen: false)
+        .checkOffExercise(workoutName, exerciseName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutData>(
-        builder: (context, value, child) => Scaffold(
-          appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.background,
-            elevation: 0,
-            leading: Icon(
-              Icons.arrow_back_ios,
-              color: Theme.of(context).colorScheme.tertiary,
-            ),
-            title: Text(
-              widget.workoutName,
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.tertiary,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Roboto',
-              ),
-            ),
-            centerTitle: true,
-          foregroundColor: Theme.of(context).colorScheme.tertiary,
-          ),
-          body: ListView.builder(
-            itemCount: value.numberOfExercisesInWorkout(widget.workoutName),
-            itemBuilder: (context, index) => ExerciseTile(
-              exerciseName: value.getReleventWorkout(widget.workoutName).exercises[index].name, 
-              weight: value.getReleventWorkout(widget.workoutName).exercises[index].weight, 
-              sets: value.getReleventWorkout(widget.workoutName).exercises[index].sets, 
-              reps: value.getReleventWorkout(widget.workoutName).exercises[index].reps, 
-              isCompleted: value.getReleventWorkout(widget.workoutName).exercises[index].isCompleted,
-            ),
-          ),
+      builder: (context, value, child) => Scaffold(
+        appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.background,
+          elevation: 0,
+          leading: Icon(
+            Icons.arrow_back_ios,
+            color: Theme.of(context).colorScheme.tertiary,
+          ),
+          title: Text(
+            widget.workoutName,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.tertiary,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Roboto',
+            ),
+          ),
+          centerTitle: true,
+          foregroundColor: Theme.of(context).colorScheme.tertiary,
+        ),
+        body: ListView.builder(
+          itemCount: value.numberOfExercisesInWorkout(widget.workoutName),
+          itemBuilder: (context, index) => ExerciseTile(
+            exerciseName: value
+                .getReleventWorkout(widget.workoutName)
+                .exercises[index]
+                .name,
+            weight: value
+                .getReleventWorkout(widget.workoutName)
+                .exercises[index]
+                .weight,
+            sets: value
+                .getReleventWorkout(widget.workoutName)
+                .exercises[index]
+                .sets,
+            reps: value
+                .getReleventWorkout(widget.workoutName)
+                .exercises[index]
+                .reps,
+            isCompleted: value
+                .getReleventWorkout(widget.workoutName)
+                .exercises[index]
+                .isCompleted,
+            onCheckBoxChanged: (val) => {
+              onCheckBoxChanged(
+                widget.workoutName,
+                value
+                    .getReleventWorkout(widget.workoutName)
+                    .exercises[index]
+                    .name,
+              )
+            },
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.background,
       ),
     );
   }

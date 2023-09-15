b6ad973987f7711cb577a5bfdbb5a9e82313a956
AnthonyBeerus <morima.anthony@gmail.com>
2023-09-15 21:06:52 +0200

    Created function for added more exercises and FAB

--- lib/Presentation Layer/pages/routine.dart
+++ lib/Presentation Layer/pages/routine.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:phenom_fitness/Data/models/workout_data.dart';
 import 'package:phenom_fitness/Domain/Models/components/exerciseTile.dart';
 
@@ -18,7 +19,21 @@ class _RoutineState extends State<Routine> {
     Provider.of<WorkoutData>(context, listen: false)
         .checkOffExercise(workoutName, exerciseName);
   }
+
   //! Fix RenderFlex bug
+  //* Make function for creating new exercise
+  Future createNewExercise(BuildContext context) {
+    return showModalBottomSheet(
+      context: context, 
+      builder: (BuildContext context) {
+          return const SizedBox(
+            height: 500,
+            child: Text('New Workout'),
+            
+        );
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutData>(
@@ -76,8 +91,68 @@ class _RoutineState extends State<Routine> {
                 )
               },
             ),
+            
           ),
-          
+          floatingActionButton:SizedBox(
+            height: 70,
+            width: 70,
+            child: Container(
+              margin: const EdgeInsets.only(
+                right: 10,
+                bottom: 10,
+              ),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  //bottom right shadow is darker
+                  BoxShadow(
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    offset: const Offset(4, 4),
+                    blurRadius: 12,
+                    spreadRadius: 1,
+                  ),
+                  //top left shadow is lighter
+                  BoxShadow(
+                    color: Theme.of(context).colorScheme.secondaryContainer,
+                    offset: const Offset(-4, -4),
+                    blurRadius: 12,
+                    spreadRadius: 1,
+                  ),
+                ],
+              ),
+              child: SpeedDial(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                foregroundColor: Theme.of(context).colorScheme.secondary,
+                animatedIcon: AnimatedIcons.add_event,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                overlayColor: Theme.of(context).colorScheme.background,
+                overlayOpacity: 0.9,
+                elevation: 0,
+                spaceBetweenChildren: 10,
+                animatedIconTheme: IconThemeData(
+                    color: Theme.of(context).colorScheme.background
+                ),
+                children: [
+                  SpeedDialChild(
+                    child: const Icon(Icons.add_circle),
+                    labelBackgroundColor:
+                        Theme.of(context).colorScheme.background,
+                    labelStyle: const TextStyle(
+                      fontFamily: "Roboto",
+                      fontSize: 18,
+                    ),
+                    label: ('Add New Routine'),
+                    backgroundColor: Theme.of(context).colorScheme.background,
+                    elevation: 10,
+                    
+                  ),
+                ],
+              ),
+
+              
+            ),
+          ),
           backgroundColor: Theme.of(context).colorScheme.background,
         ),
       ),

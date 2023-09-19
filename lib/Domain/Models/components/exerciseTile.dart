e4a4d8de5b0b7e299a3cca897cb2bd5482fd87ef
AnthonyBeerus <morima.anthony@gmail.com>
2023-09-19 14:19:11 +0200

    Added bottomSheet for Text button which show set types

--- lib/Domain/Models/components/exerciseTile.dart
+++ lib/Domain/Models/components/exerciseTile.dart
@@ -17,7 +17,6 @@ class ExerciseTile extends StatefulWidget {
     required this.reps,
     required this.isCompleted,
     required this.onCheckBoxChanged,
-
   });
 
   @override
@@ -25,30 +24,62 @@ class ExerciseTile extends StatefulWidget {
 }
 
 class _ExerciseTileState extends State<ExerciseTile> {
+  void setType(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return const SizedBox(
+            height: 400,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ListTile(
+                  title: Text('Working Set'),
+                ),
+                ListTile(
+                  title: Text('Warmup Set'),
+                ),
+                ListTile(
+                  title: Text('Failure Set'),
+                ),
+                ListTile(
+                  title: Text('Drop Set'),
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-      
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              widget.exerciseName,
-              style: const TextStyle(
-                fontSize: 20,
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                child: Align(
+                  alignment: const Alignment(-1.0, 1.0),
+                  child: Text(
+                    widget.exerciseName,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
               ),
-              
             ),
-          ),
-          SizedBox(
-            height: 120,
-            
-            child: Card(
-              margin: const EdgeInsets.symmetric(vertical: 22),
+            //*Set card
+            //TODO Make this card reuseable and duplicatable upon pressing the Add set button
+            Card(
               color: Theme.of(context).colorScheme.secondaryContainer,
               child: Slidable(
                 endActionPane: ActionPane(
@@ -64,48 +95,55 @@ class _ExerciseTileState extends State<ExerciseTile> {
                     ),
                   ],
                 ),
-                child: ListTile(
-                  leading: TextButton(
-                    onPressed: () {
-                    },
-                    child: const Text('1'),
-                  ),
-                  subtitle: SizedBox(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Chip(
-                          padding: const EdgeInsets.all(10),
-                          label: Text('${widget.weight}kg'),
-                        ),
-                        Chip(
-                          padding: const EdgeInsets.all(10),
-                          label: Text('${widget.reps}reps'),
-                        ),
-                        Chip(
-                          padding: const EdgeInsets.all(10),
-                          label: Text('${widget.sets}sets'),
-                        ),
-                      ],
+                child: Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: ListTile(
+                    leading: TextButton(
+                      //*showcases type of set
+                      onPressed: () {
+                        setType(context);
+                      },
+                      child: const Text('1'),
                     ),
+                    subtitle: SizedBox(
+                      width: 70,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Chip(
+                            padding: const EdgeInsets.all(5),
+                            label: Text('${widget.weight}kg'),
+                          ),
+                          Chip(
+                            padding: const EdgeInsets.all(5),
+                            label: Text('${widget.reps}reps'),
+                          ),
+                          Chip(
+                            padding: const EdgeInsets.all(5),
+                            label: Text('${widget.sets}sets'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    trailing: Checkbox(
+                        value: widget.isCompleted,
+                        onChanged: (value) => widget.onCheckBoxChanged!(value)),
                   ),
-                  trailing: Checkbox(
-                    value: widget.isCompleted, 
-                    onChanged: (value) => widget.onCheckBoxChanged!(value)),
                 ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: ElevatedButton( 
-              onPressed: () {
-                // Handle button press here
-              },
-              child: const Text("+ Add Set"),
+            //*Add set
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle button press here
+                },
+                child: const Text("+ Add Set"),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

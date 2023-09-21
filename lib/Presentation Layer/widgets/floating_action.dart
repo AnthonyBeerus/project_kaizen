f7e71d378eecc61d557eba57534ba5e266851fef
AnthonyBeerus <morima.anthony@gmail.com>
2023-09-21 23:23:19 +0200

    Tried to make custom FAB (failed lol)

--- lib/Presentation Layer/widgets/floating_action.dart
+++ lib/Presentation Layer/widgets/floating_action.dart
@@ -1,39 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
+class CustomFloatingActionButton extends StatelessWidget {
+  final Widget child;
+  final VoidCallback onPressed;
+  final String label;
+  final VoidCallback onTap;
+  final Icon icon;
+  final SpeedDialChild speedDialChild;
+  final List<SpeedDialChild> speedDialChildren;
 
-class FloatingAction extends StatefulWidget {
-  final Color backgroundColor;
-  final Color childIconColor;
-  final AnimatedIconData animatedIcon;
-  final List<SpeedDialChild> children;
 
-  const FloatingAction({
+  const CustomFloatingActionButton({
     super.key,
-    required this.backgroundColor,
-    required this.childIconColor,
-    required this.animatedIcon,
-    required this.children,
+    required this.child,
+    required this.onPressed,
+    required this.label,
+    required this.onTap,
+    required this.icon,
+    required this.speedDialChild,
+    required this.speedDialChildren,
   });
 
   @override
-  State<FloatingAction> createState() => _FloatingActionState();
-}
-
-class _FloatingActionState extends State<FloatingAction> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: SpeedDial(
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        overlayColor: Colors.black,
-        overlayOpacity: 0.5,
-        spacing: 1,
-        spaceBetweenChildren: 10,
-        animatedIcon: widget.animatedIcon,
-        children: widget.children,
+    return SizedBox(
+      height: 70,
+      width: 70,
+      child: Container(
+        margin: const EdgeInsets.only(
+          right: 10,
+          bottom: 10,
+        ),
+        decoration: BoxDecoration(
+          boxShadow: [
+            //bottom right shadow is darker
+            BoxShadow(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              offset: const Offset(4, 4),
+              blurRadius: 12,
+              spreadRadius: 1,
+            ),
+            //top left shadow is lighter
+            BoxShadow(
+              color: Theme.of(context).colorScheme.secondaryContainer,
+              offset: const Offset(-4, -4),
+              blurRadius: 12,
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        child: SpeedDial(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          foregroundColor: Theme.of(context).colorScheme.secondary,
+          animatedIcon: AnimatedIcons.add_event,
+          backgroundColor: Theme.of(context).colorScheme.background,
+          overlayColor: Theme.of(context).colorScheme.background,
+          overlayOpacity: 0.9,
+          elevation: 0,
+          spaceBetweenChildren: 10,
+          animatedIconTheme:
+              IconThemeData(color: Theme.of(context).colorScheme.background),
+          children: [
+            SpeedDialChild(
+              child: const Icon(Icons.add_circle),
+              labelBackgroundColor: Theme.of(context).colorScheme.background,
+              labelStyle: const TextStyle(
+                fontFamily: "Roboto",
+                fontSize: 18,
+              ),
+              label: label,
+              backgroundColor: Theme.of(context).colorScheme.background,
+              elevation: 10,
+              onTap: onTap,
+            ),
+          ],
+        ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
